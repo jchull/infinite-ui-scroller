6f789db12a41d065d75a0e6f8db77f14d07719b2
Jeremy Hull <jchull@gmail.com>
2016-04-15 09:59:35 -0600

    Added circular option, defaults to true

--- components/coderado/infinite-ui-scroller/infinite-ui-scroller.js
+++ components/coderado/infinite-ui-scroller/infinite-ui-scroller.js
@@ -1,9 +1,9 @@
 (function () {
   "use strict";
 
-      angular.module("coderado.infiniteUiScroller", [])
+  angular.module("coderado.infiniteUiScroller", [])
           
-//<nf8-ui-scroller states="ctrl.states"></nf8-ui-scroller>
+
   .directive("infiniteScroller", function(){
     return {
       scope: {},
@@ -16,29 +16,19 @@
         states: "="
       },
       link: function($scope, elem, attrs, ctrl, $timeout){
-        attrs.$observe("states", function(){
-          console.log("states list changed!");
-        });
+        ctrl.circular = (attrs.circular != "false");
 
-
+//         attrs.$observe("states", function(){
+//         });
 
         var throttledScrollEvents = _.throttle(function(event){
           var children = elem.children();
           var afterElement = children[children.length-1];
-         // console.log("afterElement: " + afterElement.getBoundingClientRect().top);
-         // console.log("offsetParent: " + afterElement.offsetParent.getBoundingClientRect().top);
 
           if(afterElement && afterElement.getBoundingClientRect().top < 8){
-            //$timeout(function(){
-            console.log("scroll next >>");
-            afterElement.offsetParent.scrollTop = 9;
-            ctrl.next();
-
-           // }, 100);
-
+            if(ctrl.next())
+              afterElement.offsetParent.scrollTop = 9;            
           }else if(elem.children()[0].offsetParent.scrollTop < 1){
-            console.log("scroll prev <<");
-
             ctrl.prev(elem.children()[0].offsetParent);
           }
 
@@ -48,28 +38,45 @@
     };
   })
 
-      .controller("InfiniteUiScrollerCtrl", function($state, $timeout){
-        var ctrl = this;
-        console.log("states: " + ctrl.states);
-        
-        // current index would be indexOf(state)
-        // size would be ctrl.states.length
-        // modulo the index by length so we go around (for now)
-        
-        ctrl.next = function next(){
-          var currentIndex = ctrl.states.indexOf($state.current.name);
-          var newStateIndex = (currentIndex + 1) % ctrl.states.length;
-          $state.go(ctrl.states[newStateIndex]);
-        }
 
-        ctrl.prev = function prev(elem){
-          var currentIndex = ctrl.states.indexOf($state.current.name);
-          var newStateIndex = (currentIndex - 1 <0)?  ctrl.states.length -1 : currentIndex - 1;
-          $state.go(ctrl.states[newStateIndex]);
-          $timeout(function(){
-            elem.scrollTop = elem.scrollHeight;
-          }, 10);
-        }
+  .controller("InfiniteUiScrollerCtrl", function($state, $timeout, $log){
+    var ctrl = this;
+    $log.info("states: " + ctrl.states);
+
+    ctrl.circular = true; // default true
+
+
+    ctrl.next = function next(){
+      var currentIndex = ctrl.states.indexOf($state.current.name);
+      var newStateIndex = 0;
+      // view not in list, go to beginning
+      if(currentIndex >= 0){
+        newStateIndex = (currentIndex + 1) % ctrl.states.length;
+      } 
         
-      })
+      if(!ctrl.circular && newStateIndex < currentIndex)
+          return false; // at the end and not circular
+
+      $log.info("scroll next >>");
+      $state.go(ctrl.states[newStateIndex]);
+      return true;
+    };
+
+
+    ctrl.prev = function prev(elem){
+      var currentIndex = ctrl.states.indexOf($state.current.name);
+      if(!ctrl.circular && currentIndex == 0)
+        return;
+
+      $log.info("scroll prev <<");
+      var newStateIndex = (currentIndex - 1 <0)?  ctrl.states.length -1 : currentIndex - 1;
+      $state.go(ctrl.states[newStateIndex]);
+      $timeout(function(){
+          elem.scrollTop = elem.scrollHeight;
+      }, 10);
+    };
+
+  })
+
 })();
+
